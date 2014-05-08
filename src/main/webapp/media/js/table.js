39f7d787663ceb92986a3d04aa2866573b5ccd7e
morninlark <nina@tesobe.com>
2014-05-08 15:45:56 +0200

    Corrected arrow direction

--- src/main/webapp/media/js/table.js
+++ src/main/webapp/media/js/table.js
@@ -4,7 +4,7 @@ $(function() {
 	$("#bankData").bind('aftertablesort', function (event, data) {
 	    var th = $(this).find("th");
 	    th.find(".arrow").remove();
-	    var arrow = data.direction === "asc" ? "↑" : "↓";
+	    var arrow = data.direction === "asc" ?  "↓" : "↑";
 	    th.eq(data.column).append('<span class="arrow">' + arrow +'</span>');
 	});
 });
